d2e496e9a88f3071af760ab8da737be3107f0a47
moboluwade <folarinboluwade.com>
2023-12-20 00:14:33 +0100

    configure redux store

--- redux/actions/todoListSlice.ts
+++ redux/actions/todoListSlice.ts
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { PayloadAction } from "@reduxjs/toolkit";
 
 interface todoListObject {
-    id: number,
+    id: string,
     todo: string,
 }
 export interface todoListState {
@@ -13,12 +13,22 @@ const initialState: todoListState = {
     value: []
 }
 
-export const todoListSlice = {
+const popTodo = (state: todoListObject[], id: string) => {
+    const staleTodoIndex = state.findIndex((todo) => { todo.id === id })
+    return [...state.slice(0, staleTodoIndex), ...state.slice(staleTodoIndex)]
+}
+
+export const todoListSlice = createSlice({
     name: 'todoList',
     initialState,
     reducers: {
-        addTodo: (state: todoListObject[], action: PayloadAction<string>) => {
-            [...state, action.payload]
-        }
+        // addTodo: (state: todoListObject[], action: PayloadAction<string>) => {
+        //     [...state, action.payload]
+        // },
+        // removeTodo: (state: todoListObject[], action: PayloadAction<string>) => {
+        //     popTodo(state, action.payload)
+        // }
     }
-}
+})
+
+// export const {addTodo, removeTodo} = todoListSlice.actions
